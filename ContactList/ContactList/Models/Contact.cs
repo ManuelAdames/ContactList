1352610baef7ef6e6a729b29327fa0c4bd62de8d
elgaryy <manuel.adames88@gmail.com>
2021-03-10 01:02:30 -0400

    Add project files.

---
+++ ContactList/ContactList/Models/Contact.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContactList.Models
+{
+    public class Contact
+    {
+        public Contact(string name, string number)
+        {
+            Name = name;
+            Number = number;
+        }
+        public string Name { get; }
+        public string Number { get; }
+    }
+}
+
---
+++ ContactList/ContactList/ViewModels/HomeViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using ContactList.Models;
+using Xamarin.Forms;
+using ContactList.Views;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Xamarin.Essentials;
+
+namespace ContactList.ViewModels
+{
+    public class HomeViewModel
+    {
+        public ObservableCollection<Contact> Contacts { get; }
+        public ICommand AddCommand { get; }
+        public ICommand DeleteCommand { get; }
+        private ICommand MoreCommmand { get; }
+
+        public HomeViewModel()
+        {
+            MoreCommmand = new Command<Contact>(OnMore);
+            AddCommand = new Command(OnAddContact);
+            DeleteCommand = new Command<Contact>(OnDeleteContact);
+
+            Contacts = new ObservableCollection<Contact>
+            {
+                //new Contact ("Maicol Martinez", "8095080000"),
+                //new Contact ("Jose", "6784390294")
+            };
+        }
+
+        private async void OnMore(Contact contact)
+        {
+            string option = await App.Current.MainPage.DisplayActionSheet(null, "Canecel", null, "Call +" + contact.Number, "Cancel");
+            if (option == "Call +" + contact.Number)
+            {
+                try
+                {
+                    PhoneDialer.Open(contact.Number);
+                }
+                catch(Exception)
+                {
+                    await App.Current.MainPage.DisplayAlert("No se pudo realizar la llamada", "Intentelo mas tarde", "Ok");
+                }
+            }
+        }
+        private async void OnAddContact()
+        {
+            await App.Current.MainPage.Navigation.PushAsync(new AddUserPage());
+            //var result = await App.Current.MainPage.DisplayPromptAsync("Add Name", "Type Name", "Ok");
+            //var phone = await App.Current.MainPage.DisplayPromptAsync("Add Number", "Type Number", "Ok");
+
+            //Contacts.Add(new Contact(result, phone));
+
+        }
+        private void OnDeleteContact(Contact contact)
+        {
+            Contacts.Remove(contact);
+        }
+    }
+}
